4ddf7fb661b168b63a78104eadf542dba2849b8b
techgeeks030715 <132001580+techgeeks030715@users.noreply.github.com>
2023-12-27 01:11:29 +0530

    Create variables.tf

---
+++ Route53/route53-module/variables.tf
@@ -0,0 +1,90 @@
+variable "zone_id" {
+    description = "This variables is used for definig the zone id"
+}
+
+variable "borrower_journey_dev_name" {
+    description = "This variable is used for definig the domain of borrower journey dev"
+}
+
+variable "borrower_journey_uat_name" {
+    description = "This variable is used for definig the domain of borrower journey uat"
+}
+
+variable "borrower_journey_prod_name" {
+    description = "This variable is used for definig the domain of borrower journey prod"
+}
+
+variable "cecrm_dev" {
+    description = "This variable is used for definig the domain of crm dev"
+}
+
+variable "cecrm_uat" {
+    description = "This variable is used for definig the domain of crm uat"
+}
+
+variable "cecrm_prod" {
+    description = "This variable is used for definig the domain of crm prod"
+}
+
+variable "document_dashboard_dev" {
+    description = "This variable is used for definig the domain of document dashboard dev"
+}
+
+variable "document_dashboard_uat" {
+    description = "This variable is used for definig the domain of document dashboard uat"
+}
+
+variable "document_dashboard_prod" {
+    description = "This variable is used for definig the domain of document dashboard prod"
+}
+
+variable "experian_dev" {
+    description = "This variable is used for definig the domain of experian dev"
+}
+
+variable "experian_uat" {
+    description = "This variable is used for definig the domain of experian uat"
+}
+
+variable "experian_prod" {
+    description = "This variable is used for definig the domain of experian prod"
+}
+
+variable "experian_model_dev" {
+    description = "This variable is used for definig the domain of experian model dev"
+}
+
+variable "experian_model_uat" {
+    description = "This variable is used for definig the domain of experian model uat"
+}
+
+variable "experian_model_prod" {
+    description = "This variable is used for definig the domain of experian model prod"
+}
+
+variable "grafana_dev" {
+    description = "This variable is used for definig the domain of grafana dev"
+}
+
+variable "grafana_uat" {
+    description = "This variable is used for definig the domain of grafana uat"
+}
+
+variable "grafana_prod" {
+    description = "This variable is used for definig the domain of grafana prod"
+}
+
+variable "dev_value" {
+    description = "This variable is used for definig the domain of dev"
+    type = list
+}
+
+variable "uat_value" {
+    description = "This variable is used for definig the domain of uat"
+    type = list
+}
+
+variable "prod_value" {
+    description = "This variable is used for definig the domain of prod"
+    type = list
+}
