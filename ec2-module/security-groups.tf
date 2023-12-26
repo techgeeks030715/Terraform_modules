daed4fcb1baf87a9329d1bbbed9c64c76b41cb8e
techgeeks030715 <132001580+techgeeks030715@users.noreply.github.com>
2023-12-27 00:10:22 +0530

    Create security-groups.tf

---
+++ ec2-module/security-groups.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "Ec2_security_group" {
+  name        = var.security_group
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_default_vpc.main.id
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
