7eb317e459b95608ac97494efc6220edc8fc29bb
techgeeks030715 <132001580+techgeeks030715@users.noreply.github.com>
2023-12-27 00:20:58 +0530

    Update ec2.tf

--- ec2-module/ec2.tf
+++ ec2-module/ec2.tf
@@ -2,5 +2,6 @@ resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  subnet_id     = "${data.aws_subnet_ids.private.ids}"
   tags          = var.resource_tags
 }
